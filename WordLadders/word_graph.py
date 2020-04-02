048f9762bf4bc16c02adb8759c9e8afb41a0b3d6
fr3632ho <fredrik@dannert.se>
2020-04-02 18:31:35 +0200

    Finished lab 2

--- WordLadders/word_graph.py
+++ WordLadders/word_graph.py
@@ -1,8 +1,12 @@
-#!/bin/python3
-
 import sys
 from collections import deque
+from collections import defaultdict
 
+'''
+# Description: Parses the given file and seperates the input into two lists; A word
+  list and a queries list.
+# Returns: Word-list and Queries-list
+'''
 def parse_data():
     inp_file = sys.stdin
     result = inp_file.read().strip().split('\n')
@@ -10,8 +14,13 @@ def parse_data():
     (N,Q) = (first_line[0],first_line[1])
     N_list = result[1:N+1]
     Q_list = result[N+1:N+1+Q]
-    return N,Q,N_list,Q_list
+    return N_list,Q_list
 
+'''
+# Description: Parses the words according to the given rules for which a vertex is connected to another
+  or not!
+# Returns: The graph containing all vertices and their outgoing edges.
+'''
 def setup_graph(words):
     copy = words.copy()
     graph = dict()
@@ -22,8 +31,7 @@ def setup_graph(words):
         for j in range(len(copy)):
             if j != i:
                 count = 0
-                word1 = list(copy[i])
-                word2 = list(copy[j])
+                word1,word2 = list(copy[i]),list(copy[j])
                 for w1 in word1[1:]:
                     prev_count = count
                     for w2 in word2:
@@ -37,43 +45,32 @@ def setup_graph(words):
                         graph[string_word1].append(copy[j])
     return graph
 
-N, Q, words, queries = parse_data()
-graph = setup_graph(words)
-#print(f'# of words: {N}, # of queries: {Q}\nWords: {words}\nQueries: {queries}\n')
-#print(f'Current graph: {graph} \n')
-
-# discovered = []
-# 'where' : ['there', 'shere']
-# 'where' --> queue.
-# discovered.append('where')
-# look_at_verices('where') --> ['there', 'shere']
-#
-# visit_vertices -- > 'there' --> goal --> end
-#     otherwise:
-
+'''
+# Description: The BFS algorithm
+# Returns: Returns the shortest amount of steps needed to get from 'start' to 'end'
+  or 'Impossible' if a path could not be found.
+'''
 def BFS(G, start, end):
-    #discovered = [False] * len(G.keys())
     if start == end:
         return 0
-    discovered = []
-    queue = []
-    discovered.append(start)
+    discovered,queue = dict(),deque([])
+    discovered[start] = 0
     queue.append([start,0])
     while queue:
         v = queue.pop()
         if v[0] == end:
             return v[1]
-        for edge in graph[v[0]]:
-            if edge not in discovered:
-                discovered.append(edge)
-                queue.append([edge,v[1] + 1])
-
+        for vertex in G[v[0]]:
+            if vertex not in discovered:
+                discovered[vertex] = 0
+                queue.appendleft([vertex,v[1] + 1])
     return "Impossible"
 
-q_list = [ i.split(" ") for i in queries]
+def run():
+    W, Q = parse_data()
+    graph = setup_graph(W)
+    queries = [ i.split(" ") for i in Q]
+    for query in queries:
+        print(BFS(graph,query[0],query[1]))
 
-for query in q_list:
-    print(BFS(graph,query[0],query[1]))
-
-
-#END
+run()
