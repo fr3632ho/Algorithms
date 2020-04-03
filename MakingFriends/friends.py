e07f855c0037e32c35e4709f955ad6b38ba74c76
fr3632ho <fredrik@dannert.se>
2020-04-03 07:08:29 +0200

    updated one line

--- MakingFriends/friends.py
+++ MakingFriends/friends.py
@@ -4,7 +4,8 @@ from collections import deque
 def parse_data():
     data = sys.stdin.read().strip().split('\n')
     data =  [line.split(' ') for line in data]
-    data = list(map(lambda x : list(map(lambda t : int(t),x)), data))
+    data = [[int(x) for x in line] for line in data]
     return int(data[0][0]),int(data[0][1]),data[1:] # N, M, data
 
 N,M,data = parse_data()
+print(data)
