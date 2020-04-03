65c19770f8bfb3407676c29bbfcdeb069e4b1bb2
fr3632ho <fredrik@dannert.se>
2020-04-03 06:55:30 +0200

    fixed the data parser

--- MakingFriends/friends.py
+++ MakingFriends/friends.py
@@ -1,2 +1,10 @@
 import sys
 from collections import deque
+
+def parse_data():
+    data = sys.stdin.read().strip().split('\n')
+    data =  [line.split(' ') for line in data]
+    data = list(map(lambda x : list(map(lambda t : int(t),x)), data))
+    return int(data[0][0]),int(data[0][1]),data[1:] # N, M, data
+
+N,M,data = parse_data()
