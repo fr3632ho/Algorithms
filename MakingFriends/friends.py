e3ea4d7938ae110c25dc99e240f2917aa0b17c5a
fr3632ho <fredrik@dannert.se>
2020-04-10 16:19:41 +0200

    Added comments to the code

--- MakingFriends/friends.py
+++ MakingFriends/friends.py
@@ -1,10 +1,11 @@
 import sys
-from math import inf
 import heapq
-from collections import deque,defaultdict
+from collections import defaultdict
 
-import random
-
+'''
+Parses the data form standard in.
+Returns: Undirected graph with the use of a defaultdict
+'''
 def parse_data():
     data = sys.stdin.read().strip().split('\n')
     data = [[int(x) for x in line.split(' ')] for line in data]
@@ -13,14 +14,14 @@ def parse_data():
     for frm,to,cost in data[1:]:
         graph[frm].append((to,cost))
         graph[to].append((frm,cost))
-        heapq.heappush(heap,(cost,frm,to))
-        heapq.heappush(heap,(cost,to,frm))
 
-    return int(data[0][0]),int(data[0][1]),graph,heap # N, M, data
-
+    return graph # N, M, data
 
 '''
-Minimal value in keys that has yet not been visited!
+Implementation of prims algorithm from the lecture.
+Data Structures: Heap for ordering the vertices, defaultdict for the graph and
+                 set for the explored and unexplored vertices.
+Returns: A total weight of all edges for the MST.
 '''
 def prim(G,root):
     #Q := Unexplored vertices, V:= Explored vertices
@@ -29,21 +30,20 @@ def prim(G,root):
     V = set()
 
     while Q:
-
         weight,u = heapq.heappop(Q)
-
+        # If vertex u is already explored then continue
         if u not in V:
+            # Add u to V, indicating that it is explored and sum up the weight
             V.add(u)
             total_weight += weight
+            #C Check all the neighbours of u and push them on the heap if unexplored
             for n,cost in G[u]:
                 if n not in V:
                     heapq.heappush(Q,(cost,n))
 
     return total_weight
 
-N,M,G,heap = parse_data()
-#print(N,M)
-#print(f'N: {N},M: {M},\nDATA: {G}\nHeap: {heap}')
+G = parse_data()
 print(prim(G,1))
 
 
