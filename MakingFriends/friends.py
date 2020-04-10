c03a737a355fd241277623ca2a4fd290d7864712
fr3632ho <fredrik@dannert.se>
2020-04-10 16:11:11 +0200

    Finished lab 3

--- MakingFriends/friends.py
+++ MakingFriends/friends.py
@@ -1,4 +1,5 @@
 import sys
+from math import inf
 import heapq
 from collections import deque,defaultdict
 
@@ -6,64 +7,45 @@ import random
 
 def parse_data():
     data = sys.stdin.read().strip().split('\n')
-    data =  [line.split(' ') for line in data]
-    data = [[int(x) for x in line] for line in data]
+    data = [[int(x) for x in line.split(' ')] for line in data]
     graph = defaultdict(list)
-    queue = []
-    for pair in data[1:]:
-        graph[pair[0]].append((pair[1],pair[2]))
-        graph[pair[1]].append((pair[0],pair[2]))
-        queue.append(pair)
+    heap = []
+    for frm,to,cost in data[1:]:
+        graph[frm].append((to,cost))
+        graph[to].append((frm,cost))
+        heapq.heappush(heap,(cost,frm,to))
+        heapq.heappush(heap,(cost,to,frm))
 
-    return int(data[0][0]),int(data[0][1]),graph # N, M, data,Q
+    return int(data[0][0]),int(data[0][1]),graph,heap # N, M, data
 
 
 '''
 Minimal value in keys that has yet not been visited!
 '''
-def minimumValue(keys,visited):
-    min = 10**12
-    min_index =-1
-
-    #This is slow!
-    for i in range(N):
-        if keys[i] < min and visited[i] == False:
-            min = keys[i]
-            min_index = i
-    return min_index
-
 def prim(G,root):
-    # visited vertices, starting min value, parents
-    visited = [False]*N
-    keys = [10**12]*N
-    keys[0] = root-1
-    #parents = [0]*N
-    #parents[0] = -1
-    for v in range(N):
+    #Q := Unexplored vertices, V:= Explored vertices
+    total_weight = 0
+    Q = [(0,root)]
+    V = set()
 
-        #Finds the index of the minimal value in keys!
-        u = minimumValue(keys,visited)
-        #Sets u as a visited node!
-        visited[u] = True
+    while Q:
 
-        neighbours = G[u+1]
+        weight,u = heapq.heappop(Q)
 
-        #print(f'u: {u+1}\nneighbours: {neighbours}\nIndex : {u}\n')
-        for n in neighbours:
+        if u not in V:
+            V.add(u)
+            total_weight += weight
+            for n,cost in G[u]:
+                if n not in V:
+                    heapq.heappush(Q,(cost,n))
 
-            v,w = n[0],n[1]
+    return total_weight
 
-            if w < keys[v-1] and not visited[v-1]:
+N,M,G,heap = parse_data()
+#print(N,M)
+#print(f'N: {N},M: {M},\nDATA: {G}\nHeap: {heap}')
+print(prim(G,1))
 
-                keys[v-1] = w
-
-    print(sum(keys[1:]))
-
-N,M,G = parse_data()
-print(N,M)
-#print(f'N: {N},M: {M},\nDATA: {G}')
-#print({1,2}.issubset({1,2,3,4}),{2,1}.issubset({1,2,3,4}))
-prim(G,1)
 
 
 
