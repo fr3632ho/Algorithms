c8bcb66e1d965f067c03a3726a30c0ac185cf395
fr3632ho <fredrik@dannert.se>
2020-05-02 16:07:50 +0200

    sped up Closes pair algorithm

---
+++ 4ClosestPair/pairs.py
@@ -0,0 +1,55 @@
+import sys
+from math import sqrt,pow
+
+def distance(i,j):
+    return sqrt(pow(i[0] - j[0],2) + pow(i[1]-j[1],2))
+'''
+Processes data and calls on the closest method producing the result
+'''
+def closest_points():
+    data = sys.stdin.read().split('\n')
+    p = [[int(x) for x in line.split(' ')] for line in data[1:-1]]
+    px,py = sorted(p,key=lambda x:x[0]),sorted(p,key=lambda x:x[1])
+    print(format(closest(px,py,len(p)),'.6f'))
+
+'''
+Finds the closest pair in a 2D plane given two sorted lists, one sorted by x and the other by y.
+Returns the distance between the closest pair!
+'''
+def closest(px,py,n):
+    if n <= 3:
+        delta = sys.maxsize
+        for i in px:
+            for j in px:
+                dist = distance(i,j)
+                if dist < delta and dist != 0:
+                    delta = dist
+        return delta
+    else:
+        mid = n//2
+        Lx,Rx = px[:mid],px[mid:]
+        Ly,Ry = py[:mid],py[mid:]
+        delta = min( closest(Lx,Ly,len(Lx)), closest(Rx,Ry,len(Rx)) )
+
+        S = []
+        r,l = py[mid][1] + delta,py[mid][1] - delta
+        for i in py[mid::-1]:
+            if i[1] < l:
+                break
+            S.append((i[0],i[1]))
+
+        for i in py[mid:]:
+            if i[1] > r:
+                break
+            S.append((i[0],i[1]))
+
+        for i in range(0,len(S)):
+            for j in range(i+1,i+7):
+                if len(S) == j:
+                    break
+                dist = distance(S[i],S[j])
+                if dist < delta and dist != 0:
+                    delta = dist
+        return delta
+
+closest_points()
