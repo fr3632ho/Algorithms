3e9de9735584a4c10768b8ce276941284d1a547d
fritjof <fr1tjof@protonmail.com>
2020-05-07 15:51:38 +0200

    finish

--- 5Gorilla/labGorilla.py
+++ 5Gorilla/labGorilla.py
@@ -3,38 +3,122 @@
 import sys
 
 def parse_data():
+	penalty = -4
+
 	x = sys.stdin.read().split('\n')
 	alphabet = x[0].split()
 	dim = range(len(alphabet))
 
-
+	# Cost matrix for aligning i with j
 	cost_matrix = [[int(j) for j in i.split()] for i in x[1:len(alphabet)+1]]
-
+	
+	# Alignment matrix map -> matrix['A']['C'] = -3
 	matrix = {}
 	for i in range(len(alphabet)):
 		matrix[alphabet[i]] = {}
 		for j in range(len(alphabet)):
 			matrix[alphabet[i]][alphabet[j]] = cost_matrix[i][j]
 
-	print(matrix)
-
+	# Queries to be evaluated. queries[0] = ['String1', 'String2']
 	queries = [[j for j in i.split()] for i in x[len(alphabet)+2:len(x) - 1]]
 
-	print(f'Finished parsing data, got the following queries: {queries} & alphabet: {alphabet}')
+	# print(f'Finished parsing data, got the following queries: {queries} & alphabet: {alphabet}')
 
+	# Looping over queries
 	for query in queries:
-		parent(query[0], query[1], cost_matrix)
+		s, t = query[0], query[1]
+		
+		# Size of strings inside query
+		n, m = len(s), len(t)
 
-def parent(s, t, c):
-	opt(len(s), len(t), s, t)
+		dp = [[-50000 for x in range(n + m + 1)] for y in range(m + n + 1)]
 
-def opt(i, j, s, t):
-	if j == 0:
-		return i * -4
-	if i == 0:
-		return j * -4
+		for i in range(n + m + 1):
+			dp[0][i] = penalty * i
+			dp[i][0] = penalty * i
+		
+		for i in range(1,n+1):
+			for j in range(1,m+1):
+
+				# Score var
+				alfa = matrix[s[i - 1]][t[j - 1]]
+
+				dp[i][j] = max(
+						dp[i - 1][j - 1] + alfa,
+						dp[i - 1][j] + penalty,
+						dp[i][j - 1] + penalty)
 
 
+
+		l = n + m
+		i, j = n, m
+		ys = [0]*(l + 1)
+		xs = [0]*(l + 1)
+		xpos = l
+		ypos = l
+
+		# for row in dp:
+		# 	print(row)
+
+
+		while not (i == 0 or j == 0):
+
+			# print(f's[i - 1] = {s[i - 1]}' )
+			# print(f't[j - 1] = {t[j - 1]}' )
+			# print(f't = {t}' )
+			# print(f's = {s}' )
+			# print(f'xs = {xs}' )
+			# print(f'dp[{i}][{j}] = {dp[i][j]} \n' )
+
+			# if s[i - 1] == t[j - 1]:
+			# 	xs[xpos] = s[i - 1]
+			# 	ys[ypos] = t[j - 1]
+			# 	xpos, ypos, i, j = dec(1, xpos, ypos, i, j)
+
+			if (dp[i - 1][j - 1] + matrix[s[i - 1]][t[j - 1]]) == dp[i][j]:
+				xs[xpos] = s[i - 1]
+				ys[ypos] = t[j - 1]
+				xpos, ypos, i, j = dec(1, xpos, ypos, i, j)
+
+			elif (penalty + dp[i - 1][j]) == dp[i][j]:
+				xs[xpos] = s[i - 1]
+				ys[ypos] = '*'
+				xpos, ypos, i = dec(1, xpos, ypos, i)
+
+			else:# (penalty + dp[i][j - 1]) == dp[i][j]:
+				xs[xpos] = '*'
+				ys[ypos] = t[j - 1]
+				xpos, ypos, j = dec(1, xpos, ypos, j)
+
+		while xpos > 0:
+			if i > 0:
+				xs[xpos] = s[i - 1]
+				i -= 1 
+			else: 
+				xs[xpos] = '*'
+
+			xpos -= 1
+
+		while ypos > 0:
+			if j > 0:
+				ys[ypos] = t[j - 1]
+				j -= 1
+			else:
+				ys[ypos] = '*'
+
+			ypos -= 1
+
+		id = 1
+		for i in range(min(len(xs), len(ys)), 0, -1):
+			if ys[i - 1] == '*' and xs[i - 1] == '*':
+				id = i
+				break
+
+		print("".join(xs[id:]), "".join(ys[id:]))
+	
+def dec(val, *args):
+	for i in args:
+		yield i - val
 
 def main():
 	parse_data()
--- 5Gorilla/testmatrix.py
+++ 5Gorilla/testmatrix.py
@@ -1,7 +1,8 @@
 cost_matrix = [[2, 0, -1], [0, 3, 1], [-1, 1, 3]]
-matrix = {}
+
 alpha = ['A', 'B', 'C']
 
+matrix = {}
 for i in range(len(alpha)):
 	matrix[alpha[i]] = {}
 	for j in range(len(alpha)):
